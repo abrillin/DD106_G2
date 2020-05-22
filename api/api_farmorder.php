8306979166d843e0db6516fe535ab130fceadc94
Giemini <4a455028@stust.edu.tw>
2020-05-22 12:49:17 +0800

    1249

--- api/api_farmorder.php
+++ api/api_farmorder.php
@@ -1,6 +1,6 @@
 <?php
-// require_once("connectDB.php");
-// $sql = "insert into `item`(`no`, `name`, `price`, `description`, `status`, `img`, `date`, `seller_no`) values(:no, :name, :price, :description, '0', :img, :date, :seller_no)";
-// $commodity = $pdo->prepare($sql);
+require_once("connectDB.php");
+$sql = "insert into `item`(`no`, `name`, `price`, `description`, `status`, `img`, `date`, `seller_no`) values(:no, :name, :price, :description, '0', :img, :date, :seller_no)";
+$commodity = $pdo->prepare($sql);
 
 ?>
