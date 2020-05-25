b323ff6dd8d3807c4edc2dd0219a5d4ef66c2d17
underdog618 <underdog618@gmail.com>
2020-05-25 16:58:14 +0800

    [FIX]部分頁面錯誤修正

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -4,7 +4,9 @@
       <button id="scroll_btn">&#9660;</button>
       <div id="scroll_section">
         <button id="scroll_top" class="scroll_section_btn">Top</button>
-        <button id="scroll_knowledge" class="scroll_section_btn">知識百科</button>
+        <button id="scroll_knowledge" class="scroll_section_btn">
+          知識百科
+        </button>
         <button id="scroll_market" class="scroll_section_btn">水果市集</button>
         <button id="scroll_blog" class="scroll_section_btn">果農日誌</button>
         <button id="scroll_member" class="scroll_section_btn">會員中心</button>
@@ -13,9 +15,9 @@
       <div id="mainLOGO">
         <img src="../assets/LOGO.svg" alt />
       </div>
-      <div
-        id="mainText"
-      >以「水果王國」名聞全世界的台灣，水果種類豐富，一年四季皆可嚐到新鮮可口的水果，但多數人可能不知道這些水果的產地、產季、甚至是色香味等等，因此希望能藉由我們的網站帶大家認識台灣獨特的水果文化。</div>
+      <div id="mainText">
+        以「水果王國」名聞全世界的台灣，水果種類豐富，一年四季皆可嚐到新鮮可口的水果，但多數人可能不知道這些水果的產地、產季、甚至是色香味等等，因此希望能藉由我們的網站帶大家認識台灣獨特的水果文化。
+      </div>
       <div id="knowledge_container">
         <div class="season">
           <div class="spring_tab tab -on" data-target="tab1">春</div>
@@ -28,7 +30,9 @@
             <div class="knowledge_page_back">
               <div class="knowledge_page_title">水果介紹</div>
               <div class="knowledge_page_content">
-                <p>以「水果王國」名聞全世界的台灣，水果種類豐富，一年四季皆可嚐到新鮮可口的水果，但多數人可能不知道這些水果的產地、產季、甚至是色香味等等，因此希望能藉由我們的網站帶大家認識台灣獨特的水果文化。</p>
+                <p>
+                  以「水果王國」名聞全世界的台灣，水果種類豐富，一年四季皆可嚐到新鮮可口的水果，但多數人可能不知道這些水果的產地、產季、甚至是色香味等等，因此希望能藉由我們的網站帶大家認識台灣獨特的水果文化。
+                </p>
                 <div class="knowledge_page_content_spring"></div>
               </div>
             </div>
@@ -52,7 +56,9 @@
           <div class="knowledge_page tab3">
             <div class="knowledge_page_back">
               <div class="knowledge_page_title">果農日誌</div>
-              <div class="knowledge_page_content knowledge_page_content_fall"></div>
+              <div
+                class="knowledge_page_content knowledge_page_content_fall"
+              ></div>
             </div>
           </div>
           <div class="knowledge_page tab4">
@@ -112,7 +118,7 @@
         </div>
       </div>
       <div id="blog_container">
-        <router-link to="/main/blog-landing">
+        <router-link to="/main/blog">
           <button class="home_title">果農日誌</button>
         </router-link>
         <div id="blog_page">
@@ -138,7 +144,7 @@
         </div>
       </div>
       <div id="member_container">
-        <router-link to="/main/member/information">
+        <router-link to="/main/member/Information">
           <button class="home_title">會員中心</button>
         </router-link>
         <div id="member_page">
