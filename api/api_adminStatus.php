94815bf6daefc51bfbd3cbcbb4aa4d8abc84caf2
lesliezsy <56362727+lesliezsy@users.noreply.github.com>
2020-05-26 02:02:02 +0800

    後台管理員登出

--- api/api_adminStatus.php
+++ api/api_adminStatus.php
@@ -4,6 +4,9 @@ if (isset($_SESSION["admin_no"])) {
     $member = array(
         "no" => $_SESSION["admin_no"],
         "name" => $_SESSION["admin_name"],
+
+        // "status" => $_SESSION["admin_status"]
+
     );
     echo json_encode($member);
 } else {
